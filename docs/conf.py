6e668591bdcd22fe97f2872263f28359da50601c
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2023-04-06 19:03:53 +0100

    Starting sphinx docs support.

---
+++ docs/conf.py
@@ -0,0 +1,90 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'ncdata'
+copyright = '2023, pp-mo'
+author = 'pp-mo'
+
+# The full version, including alpha/beta/rc tags
+release = '0.01'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinxcontrib.napoleon"
+]
+
+intersphinx_mapping = {
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "dask": ("https://docs.dask.org/en/stable/", None),
+    "xarray": ("https://docs.xarray.dev/en/stable/", None),
+    # Doesn't work as expected ?
+    "iris": ("https://scitools-iris.readthedocs.io/en/latest/", None),
+    # Can't make this work ??
+    # "netCDF4": ("https://github.com/Unidata/netcdf4-python", None),
+}
+
+
+from pathlib import Path
+docsdir_pth = Path(__name__).parent.absolute()
+print('docsdir import path:', docsdir_pth)
+ncdata_pth = (docsdir_pth.parent / 'lib').absolute()
+print('ncdata import path:', ncdata_pth)
+
+import sys
+sys.path.append(str(ncdata_pth))
+print('PATH:')
+print('\n'.join(p for p in sys.path))
+
+# Autodoc config..
+autopackage_name = [
+    'ncdata',
+    'ncdata.iris_xarray',
+    'ncdata.netcdf4',
+    'ncdata.xarray',
+    'ncdata.dataset_like'
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'pydata_sphinx_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,23 @@
+.. ncdata documentation master file, created by
+   sphinx-quickstart on Thu Apr  6 17:33:47 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+ncdata
+======
+
+.. automodule:: ncdata
+
+
+.. toctree::
+   :maxdepth: 2
+
+   api/modules
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
