8ec8e6906024b5ce771b7ad7db2e803f815dab80
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2023-04-20 18:16:57 +0100

    Minimal setup.py; TBD - replace with pyproject.toml

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ncdata',
+    version='0.0.1',
+    url='https://github.com/pp-mo/ncdata.git',
+    author='pp-mo',
+    author_email='patrick.peglar@metoffice.gov.uk',
+    description='NetCDF data interoperability between Iris and Xarray',
+    packages=find_packages(),
+    install_requires=['numpy'],
+)
