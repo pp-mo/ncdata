2fe02a8f922c251aa97f0e18079a58536f2117da
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2023-04-25 17:19:47 +0100

    Small changes; more docstrings.

--- lib/ncdata/_core.py
+++ lib/ncdata/_core.py
@@ -11,7 +11,7 @@ Current limitations :
 of dimensions referenced by variables.
 
 """
-from typing import Dict, Optional, Tuple
+from typing import Dict, Optional, Tuple, Union
 
 import numpy as np
 
@@ -21,6 +21,11 @@ import numpy as np
 
 
 class NcData:
+    """
+    A group- or dataset-level container, containing dimensions, variables, attributes
+    and sub-groups.
+    """
+
     def __init__(
         self,
         name: Optional[str] = None,
@@ -29,6 +34,7 @@ class NcData:
         attributes: Dict[str, "NcAttribute"] = None,
         groups: Dict[str, "NcData"] = None,
     ):
+        #: a group/dataset name : optional
         self.name: str = name
         self.dimensions: Dict[str, "NcDimension"] = dimensions or {}
         self.variables: Dict[str, "NcVariable"] = variables or {}
@@ -37,23 +43,60 @@ class NcData:
 
 
 class NcDimension:
+    """
+    A dimension associates a length with a name.
+    A length of 0 indicates an "unlimited" dimension, though that is essentially a
+    file-specific concept.
+
+    TODO: I think the unlimited interpretation is limiting, since we will want to
+     represent "current length" too :  Change this by defining a boolean with
+     'is_unlimited' meaning.
+    """
+
     def __init__(self, name: str, size: int = 0):
         self.name: str = name
         self.size: int = size  # N.B. we retain the 'zero size means unlimited'
 
 
 class NcVariable:
+    """
+        A variable with dimensions, dtype and data, and attributes.
+
+        'data' may be None, but if not is expected to be an array : either numpy (real) or
+        Dask (lazy).
+
+        The 'dtype' will presumably should match the data, if any.
+
+        It has no 'shape' property, in practice this might be inferred from either the
+        data or dimensions.  If the dims are empty, it is a scalar.
+
+        A variable makes no effort to ensure that its dimensions, dtype + data are
+        consistent.  This is to be managed by the creator.
+    ="""
+
     def __init__(
         self,
         name: str,
-        dimensions: Tuple[str] = None,
-        data: np.ndarray = None,
+        dimensions: Tuple[str] = (),
+        # NOTE: flake8 objects to type checking against an unimported package, even in
+        # a quoted reference when it's not otherwise needed (and we don't want to
+        # import it).
+        # TODO: remove the flake8 annotation if this gets fixed.
+        data: Optional[
+            Union[np.ndarray, "dask.array.array"]  # noqa: F821
+        ] = None,
         dtype: np.dtype = None,
         attributes: Dict[str, "NcAttribute"] = None,
         group: "NcData" = None,
     ):
+        """
+        Create a variable.  The 'dtype' arg relevant only when no data is provided :
+        If 'data' is provided, it is converted to an array if needed, and its dtype
+        replaces any provided 'dtype'.
+        """
+        #: variable name
         self.name = name
-        self.dimensions = tuple(dimensions or ())
+        self.dimensions = tuple(dimensions)
         if data is not None:
             if not hasattr(data, "dtype"):
                 data = np.asanyarray(data)
@@ -74,6 +117,16 @@ class NcVariable:
 
 
 class NcAttribute:
+    """
+    An object representing a variable or dataset attribute.
+    Associates a name to a value which is either a numpy 1-D array or scalar.
+
+    We expect the value to be 0- or 1-dimensional, and an allowed dtype.
+    However none of this is checked.
+
+    In an actual netcdf dataset, a "scalar" is actually just an array of length 1.
+    """
+
     def __init__(self, name: str, value):
         self.name: str = name
         # Attribute values are arraylike, have dtype
@@ -83,9 +136,32 @@ class NcAttribute:
         self.value: np.ndarray = value
 
     def _as_python_value(self):
+        """
+        Return the content, but converting any character data to Python strings.
+
+        An array of 1 value returns as a single scalar value, since this is how
+        attributes behave in actual netcdf data.
+
+        We don't bother converting numeric data, since numpy scalars are generally
+        interchangeable in use with Python ints or floats.
+        """
         result = self.value
+        # Attributes are either scalar or 1-D
+        if result.ndim >= 2:
+            raise ValueError(
+                "Attribute value should only be 0- or 1-dimensional."
+            )
+
+        if result.shape == (1,):
+            result = result[0]
+
         if result.dtype.kind in ("U", "S"):
-            result = str(result)
-            if isinstance(result, bytes):
-                result = result.decode()
+            if result.ndim == 0:
+                result = str(result)
+            elif result.ndim == 1:
+                result = [str(x) for x in result]
+
+        # TODO: is this a possibiliity ?
+        # if isinstance(result, bytes):
+        #     result = result.decode()
         return result
