7c7f1f97fcbc9c4f941a816a2827064c0d450a81
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2023-04-25 17:25:02 +0100

    Added simple unit tests for core objects.

---
+++ lib/tests/unit/core/test_NcAttribute.py
@@ -0,0 +1,101 @@
+"""
+Tests for class :class:`ncdata.NcAttribute`.
+
+Very simple for now, but we may add more behaviour in future.
+"""
+import numpy as np
+import pytest
+
+from ncdata import NcAttribute
+
+_attribute_testdata = {
+    "int": [1, 2, 3, 4],
+    "float": [1.2, 3.4, 5.6],
+    "string": ["xx", "yyy", "z"],
+}
+_attribute_test_types = _attribute_testdata.keys()
+
+_data_types = [
+    "int",
+    "float",
+    "string",
+    "numpyint",
+    "numpyfloat",
+    "numpystring",
+]
+_container_types = ["scalar", "vectorof1", "vectorofN"]
+
+
+@pytest.fixture(params=_data_types)
+def datatype(request):
+    return request.param
+
+
+@pytest.fixture(params=_container_types)
+def structuretype(request):
+    return request.param
+
+
+def attrvalue(datatype, structuretype):
+    # get basic type = 'int', 'float', 'string'
+    (datatype,) = (
+        key for key in _attribute_testdata.keys() if key in datatype
+    )
+
+    # fetch a set of values
+    values = _attribute_testdata[datatype]
+
+    # pass as an array if specified
+    if "numpy" in datatype:
+        values = np.array(values)
+
+    # pass a single value, or a list of 1, or a list of N
+    if structuretype == "scalar":
+        result = values[0]
+    elif structuretype == "vectorof1":
+        result = values[:1]
+    else:
+        assert structuretype == "vectorofN"
+        result = values
+    return result
+
+
+class Test_NcAttribute__init__:
+    def test_value(self, datatype, structuretype):
+        value = attrvalue(datatype, structuretype)
+
+        attr = NcAttribute("x", value=value)
+        expected_array = np.asanyarray(value)
+
+        assert isinstance(attr.value, np.ndarray)
+        assert np.all(attr.value == expected_array)
+        if hasattr(value, "dtype"):
+            # An actual array is stored directly, i.e. without copying
+            assert attr.value is value
+
+
+class Test_NcAttribute__as_python_value:
+    def test_value(self, datatype, structuretype):
+        value = attrvalue(datatype, structuretype)
+
+        attr = NcAttribute("x", value)
+        result = attr._as_python_value()
+
+        # Note: "vectorof1" data should appear as *scalar*.
+        is_vector = "vectorofN" in structuretype
+        is_string = "string" in datatype
+        if is_string:
+            # String data is returned as (lists of) strings
+            if is_vector:
+                assert isinstance(result, list)
+                assert all(isinstance(x, str) for x in result)
+            else:
+                assert isinstance(result, str)
+        else:
+            # Numeric data is returned as arrays or array scalars.
+            if is_vector:
+                assert isinstance(result, np.ndarray)
+                assert result.ndim == 1
+            else:
+                # array scalar
+                assert result.shape == ()
---
+++ lib/tests/unit/core/test_NcVariable.py
@@ -0,0 +1,125 @@
+"""Tests for class :class:`ncdata.NcVariable`.
+
+There is almost no actual behaviour, but we can test some constructor behaviours,
+such as valid calling options and possibilities for data and dtype.
+"""
+import dask.array as da
+import numpy as np
+import pytest
+
+from ncdata import NcData, NcVariable
+
+
+class Test_NcVariable__init__:
+    def test_minimum_args(self):
+        # We can create a variable with no args.
+        name = "varname"
+        sample = NcVariable("varname")
+        # No data, no dtype.  Variables don't have 'shape' anyway
+        assert sample.name is name
+        assert sample.dimensions == ()
+        assert sample.data is None
+        assert sample.dtype is None
+        assert sample.attributes == {}
+
+    def test_never_nameless(self):
+        # Can't create a variable without a name.
+        msg = "required positional argument: 'name'"
+        with pytest.raises(TypeError, match=msg):
+            _ = NcVariable()
+
+    def test_allargs(self):
+        # Since there is no type checking, you can put whatever you like in the "slots".
+        name = "data_name"
+        dims = {f"dimname_{i}": f"dim_{i}" for i in range(3)}
+        vars = {f"varname_{i}": f"var_{i}" for i in range(5)}
+        attrs = {f"varname_{i}": f"var_{i}" for i in range(6)}
+        grps = {f"groupname_{i}": f"group_{i}" for i in range(3)}
+        sample1 = NcData(
+            name=name,
+            dimensions=dims,
+            variables=vars,
+            attributes=attrs,
+            groups=grps,
+        )
+        # Nothing is copied : all contents are simply references to the inputs
+        assert sample1.name is name
+        assert sample1.dimensions is dims
+        assert sample1.variables is vars
+        assert sample1.groups is grps
+        assert sample1.attributes is attrs
+        # Also check construction with arguments alone (no keywords).
+        sample2 = NcData(name, dims, vars, attrs, grps)
+        # result is new object, but all contents are references identical to the other
+        assert sample2 is not sample1
+        for name in [n for n in dir(sample1) if n[0] != "_"]:
+            assert getattr(sample2, name) is getattr(sample1, name)
+
+    @pytest.mark.parametrize("with_data", [False, True])
+    def test_dtype__with_and_without_data(self, with_data):
+        test_dtype_nonfunctional = "invalid_dtype"
+        data = np.arange(2.0) if with_data else None
+        var = NcVariable("x", data=data, dtype=test_dtype_nonfunctional)
+        expect_dtype = data.dtype if with_data else test_dtype_nonfunctional
+        assert var.dtype == expect_dtype
+
+    @pytest.mark.parametrize(
+        # All of these cases should deliver data wrapped as np.asanyarray()
+        "data",
+        [
+            12,
+            3.4,
+            "a",
+            "bcd",
+            [1, 2],
+            [(3.4, 4), (5, 6)],
+            ["xxx", "yyy", "zzz"],
+        ],
+    )
+    def test_data__pythontypes(self, data):
+        var = NcVariable("x", data=data, dtype="ineffective")
+        expect_array = np.asanyarray(data)
+        assert isinstance(var.data, np.ndarray)
+        assert var.dtype == expect_array.dtype
+        assert np.all(var.data == expect_array)
+        if isinstance(data, np.ndarray):
+            # Actual array data is stored directly
+            assert var.data is data
+
+    def test_data__listsofstrings(self):
+        # handling of a ragged string-length is a specific case worth exploring
+        data = [["one", "two", "three"], ["4", "5", "6"]]
+        var = NcVariable("x", data=data)
+        assert isinstance(var.data, np.ndarray)
+        assert var.data.shape == (2, 3)
+        assert var.dtype == var.data.dtype == np.dtype("U5")
+
+    @pytest.mark.parametrize(
+        "dtype", ["i4", "i8", "f4", "f8", "u1", "i1", "U4"]
+    )
+    def test_data__numpy(self, dtype):
+        elem = "abc" if dtype.startswith("U") else 12
+        dtype = np.dtype(dtype)
+        arr = np.zeros((2, 3), dtype=dtype)
+        arr[:] = elem
+        # The variable should preserve the original of a contained numpy array, and adopt
+        # it's dtype as it's own
+        var = NcVariable("x", data=arr)
+        assert var.data is arr  # not copied or re-formed
+        assert var.dtype == dtype
+
+    @pytest.mark.parametrize("dtype", ["i4", "i8", "f4", "f8", "u1", "i1"])
+    def test_data__numpy_scalar(self, dtype):
+        # As above, but for scalars.
+        dtype = np.dtype(dtype)
+        value = np.array(1.0, dtype=dtype)
+        var = NcVariable("x", data=value)
+        assert var.data is value
+        assert var.dtype == dtype
+
+    def test_data__dask(self):
+        # Check that lazy data is also contained directly, without adjustment or
+        # modification
+        arr = da.ones((2, 3))
+        var = NcVariable("x", data=arr)
+        assert var.data is arr
