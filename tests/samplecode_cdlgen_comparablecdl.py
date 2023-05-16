d49bf8baf698b1d29e04cf618c0faebaced86744
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2023-05-16 15:54:34 +0100

    Core unit tests (#3)
    Only the beginnings

---
+++ tests/samplecode_cdlgen_comparablecdl.py
@@ -0,0 +1,181 @@
+"""
+Tests for :mod:`tests.unit.netcdf._compare_nc_files`
+
+Yes I know, tests of tests.  But it seems necessary.
+"""
+import os
+import shutil
+from pathlib import Path
+import subprocess
+from tempfile import mkdtemp
+from typing import AnyStr, Optional, List
+
+
+# Note : `_env_bin_path` and `ncgen_from_cdl` are taken from Iris test code.
+# Code here duplicated from Iris v3.4.1
+# https://github.com/SciTools/iris/blob/v3.4.1/lib/iris/tests/stock/netcdf.py#L18-L63
+
+def _env_bin_path(exe_name: AnyStr = None):
+    """
+    Return a Path object for (an executable in) the environment bin directory.
+
+    Parameters
+    ----------
+    exe_name : str
+        If set, the name of an executable to append to the path.
+
+    Returns
+    -------
+    exe_path : Path
+        A path to the bin directory, or an executable file within it.
+
+    Notes
+    -----
+    For use in tests which spawn commands which should call executables within
+    the Python environment, since many IDEs (Eclipse, PyCharm) don't
+    automatically include this location in $PATH (as opposed to $PYTHONPATH).
+    """
+    exe_path = Path(os.__file__)
+    exe_path = (exe_path / "../../../bin").resolve()
+    if exe_name is not None:
+        exe_path = exe_path / exe_name
+    return exe_path
+
+NCGEN_PATHSTR = str(_env_bin_path("ncgen"))
+
+
+def ncgen_from_cdl(
+    cdl_str: Optional[str], cdl_path: Optional[str], nc_path: str
+):
+    """
+    Generate a test netcdf file from cdl.
+
+    Source is CDL in either a string or a file.
+    If given a string, will either save a CDL file, or pass text directly.
+    A netcdf output file is always created, at the given path.
+
+    Parameters
+    ----------
+    cdl_str : str or None
+        String containing a CDL description of a netcdf file.
+        If None, 'cdl_path' must be an existing file.
+    cdl_path : str or None
+        Path of temporary text file where cdl_str is written.
+        If None, 'cdl_str' must be given, and is piped direct to ncgen.
+    nc_path : str
+        Path of temporary netcdf file where converted result is put.
+
+    Notes
+    -----
+    For legacy reasons, the path args are 'str's not 'Path's.
+
+    """
+    if cdl_str and cdl_path:
+        with open(cdl_path, "w") as f_out:
+            f_out.write(cdl_str)
+    if cdl_path:
+        # Create netcdf from stored CDL file.
+        call_args = [NCGEN_PATHSTR, "-k3", "-o", nc_path, cdl_path]
+        call_kwargs = {}
+    else:
+        # No CDL file : pipe 'cdl_str' directly into the ncgen program.
+        if not cdl_str:
+            raise ValueError("Must provide either 'cdl_str' or 'cdl_path'.")
+        call_args = [NCGEN_PATHSTR, "-k3", "-o", nc_path]
+        call_kwargs = dict(input=cdl_str, encoding="ascii")
+
+    subprocess.run(call_args, check=True, **call_kwargs)
+
+
+# CDL to create a reference file with "all" features included.
+_base_cdl = """
+netcdf everything {
+dimensions:
+	x = 2 ;
+	y = 3 ;
+	strlen = 5 ;
+variables:
+	int x(x) ;
+		x:name = "var_x" ;
+	int var_2d(x, y) ;
+	uint var_u8(x) ;
+	float var_f4(x) ;
+	double var_f8(x) ;
+	char var_str(x, strlen) ;
+	int other(x) ;
+	    other:attr_int = 1 ;
+	    other:attr_float = 2.f ;
+	    other:attr_double = 2. ;
+	    other:attr_string = "this" ;
+    int masked_int(y) ;
+        masked_int:_FillValue = -3 ;
+    int masked_float(y) ;
+        masked_float:_FillValue = -4 ;
+        
+// global attributes:
+		:global_attr_1 = "one" ;
+		:global_attr_2 = 2 ;
+
+group: grp_1 {
+    dimensions:
+        y = 7 ;
+    variables:
+        int parent_dim(x) ;
+        int own_dim(y) ;
+} // group grp_1
+
+group: grp_2 {
+    variables:
+        int grp2_x(x) ;
+} // group grp_2
+}
+"""
+
+
+def comparable_cdl(text:str) -> List[str]:
+    """
+    Convert a CDL string to a list of stripped lines, with certain problematic things
+    removed.  The resulting list of strings should be comparable between identical
+    netcdf files.
+
+    """
+    lines = text.split('\n')
+    lines = [
+        line.strip(' \t\n')
+        for line in lines
+    ]
+    lines = [
+        line
+        for line in lines
+        if (
+                len(line)
+                # Exclude global pseudo-attribute (some versions of ncdump)
+                and ':_NCProperties =' not in line
+        )
+    ]
+    # Also exclude the first line, which includes the filename
+    hdr, lines = lines[0], lines[1:]
+    assert hdr.startswith('netcdf') and hdr[-1] == "{"
+    return lines
+
+
+# This is a self-testing routine for the test utility function (!)
+def test_gen():
+    from pathlib import Path
+
+    tmp_dirpath = mkdtemp()
+    try:
+        cdl_path = tmp_dirpath / "tmp_nccompare_test.cdl"
+        nc_path = tmp_dirpath / "tmp_nccompare_test.nc"
+
+        ncgen_from_cdl(cdl_str=_base_cdl, cdl_path=cdl_path, nc_path=nc_path)
+        bytes = subprocess.check_output(['ncdump', '-h', nc_path])
+
+        cdl_lines = comparable_cdl(_base_cdl)
+        dump_lines = comparable_cdl(bytes.decode())
+        assert cdl_lines == dump_lines
+        # os.system('ncdump -h ' + str(nc_path))
+
+    finally:
+        shutil.rmtree(tmp_dirpath)
+
---
+++ tests/unit/core/test_NcData.py
@@ -0,0 +1,44 @@
+"""Tests for class :class:`ncdata.NcData`.
+
+There is almost no behaviour, but we can test some constructor usages.
+"""
+from ncdata import NcData
+
+
+class Test_NcData__init__:
+    def test_noargs(self):
+        sample = NcData()
+        assert sample.name is None
+        assert sample.dimensions == {}
+        assert sample.variables == {}
+        assert sample.groups == {}
+        assert sample.attributes == {}
+
+    def test_allargs(self):
+        # Since there is no type checking, you can put what you like in the "slots".
+        name = "data_name"
+        dims = {f"dimname_{i}": f"dim_{i}" for i in range(3)}
+        vars = {f"varname_{i}": f"var_{i}" for i in range(5)}
+        attrs = {f"varname_{i}": f"var_{i}" for i in range(6)}
+        grps = {f"groupname_{i}": f"group_{i}" for i in range(3)}
+        sample1 = NcData(
+            name=name,
+            dimensions=dims,
+            variables=vars,
+            attributes=attrs,
+            groups=grps,
+        )
+        # Nothing is copied : all contents are simply references to the inputs
+        assert sample1.name is name
+        assert sample1.dimensions is dims
+        assert sample1.variables is vars
+        assert sample1.groups is grps
+        assert sample1.attributes is attrs
+
+        # Also check construction with arguments alone (no keywords).
+        sample2 = NcData(name, dims, vars, attrs, grps)
+        # result is a new object, but contents are references identical to the other
+        assert sample2 is not sample1
+        for name in dir(sample1):
+            if not name.startswith("_"):
+                assert getattr(sample2, name) is getattr(sample1, name)
