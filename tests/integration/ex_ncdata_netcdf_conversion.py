2dc94ffce5193dbec868fe2f628bb217af93f73a
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2023-05-31 02:31:42 +0100

    Examples tidy up their temporary files.

--- tests/integration/ex_ncdata_netcdf_conversion.py
+++ tests/integration/ex_ncdata_netcdf_conversion.py
@@ -3,7 +3,9 @@ A proof-of-concept example workflow for :mod:`ncdata.netcdf4`.
 
 Showing loading and saving ncdata to/from netcdf files.
 """
+import tempfile
 from pathlib import Path
+from shutil import rmtree
 
 import netCDF4 as nc
 import numpy as np
@@ -16,27 +18,41 @@ from tests import testdata_dir
 
 def example_nc4_load_save_roundtrip():  # noqa: D103
     # Load an existing file, save-netcdf4 : check same (with Iris for now)
+    print('\n----\nNetcdf4 load-save example.')
+
     filepath = testdata_dir / "toa_brightness_temperature.nc"
     ncdata = from_nc4(filepath)
-    filepath2 = Path("./temp_nc_output.nc").absolute()
-    to_nc4(ncdata, filepath2)
 
-    # Convert to Iris + compare (a bit of a cheat, bit OK for now?)
-    import iris
+    tempdir_path = Path(tempfile.mkdtemp())
+    try:
+        filepath2 = tempdir_path / "temp_nc_output.nc"
+        to_nc4(ncdata, filepath2)
 
-    cube1 = iris.load_cube(filepath)
-    cube2 = iris.load_cube(filepath2)
-    print("Round-tripped result, as iris cube:")
-    print(cube2)
-    print("\nold-file-cube == new-file-cube ? ", cube1 == cube2)
-    assert cube1 == cube2
+        # Convert to Iris + compare (a bit of a cheat, bit OK for now?)
+        import iris
 
-    equals_result = compare_nc_files(filepath, filepath2) == []
-    print("\nFiles compare? :", equals_result)
-    assert equals_result
+        cube1 = iris.load_cube(filepath)
+        cube2 = iris.load_cube(filepath2)
+        print("Round-tripped result, as iris cube:")
+        print(cube2)
+        print("\nold-file-cube == new-file-cube ? ", cube1 == cube2)
+        assert cube1 == cube2
+
+        equals_result = compare_nc_files(filepath, filepath2) == []
+        print("\nFiles compare? :", equals_result)
+        assert equals_result
+
+    finally:
+        rmtree(tempdir_path)
+
+    print('\n== Netcdf4 load-save roundtrip finished OK.\n')
+
 
 
 def example_nc4_save_reload_unlimited_roundtrip():
+    # Create arbitrary ncdata, save to netcdf4, re-load and check.
+    print('\n----\nNetcdf4 save-load example.')
+
     ncdata = NcData()
     len_x, len_y = 4, 2
     ncdata.dimensions["dim_x"] = NcDimension("dim_x", len_x, unlimited=True)
@@ -53,45 +69,53 @@ def example_nc4_save_reload_unlimited_roundtrip():
     print("Source ncdata object:")
     print(ncdata)
 
-    print("")
-    filepath = Path("./temp_nc_2_unlim.nc").absolute()
-    print("Saving to file ", filepath)
-    to_nc4(ncdata, filepath)
-    import os
+    tempdir_path = Path(tempfile.mkdtemp())
+    try:
+        print("")
+        filepath = tempdir_path / "temp_nc_2_unlim.nc"
+        print("Saving to file ", filepath)
+        to_nc4(ncdata, filepath)
+        import os
 
-    print("\n\nCreated output:")
-    os.system(f"ncdump -h {filepath!s}")
-    print("\n")
+        print("\n\nCreated output:")
+        os.system(f"ncdump -h {filepath!s}")
+        print("\n")
 
-    ds = nc.Dataset(filepath)
-    assert list(ds.dimensions.keys()) == ["dim_x", "dim_y"]
-    assert ds.dimensions["dim_x"].isunlimited()
+        ds = nc.Dataset(filepath)
+        assert list(ds.dimensions.keys()) == ["dim_x", "dim_y"]
+        assert ds.dimensions["dim_x"].isunlimited()
 
-    assert list(ds.variables.keys()) == ["var_x"]
-    assert ds.variables["var_x"].ncattrs() == ["varattr1"]
+        assert list(ds.variables.keys()) == ["var_x"]
+        assert ds.variables["var_x"].ncattrs() == ["varattr1"]
 
-    assert ds.ncattrs() == ["globalattr1"]
-    assert ds.getncattr("globalattr1") == "one"
-    ds.close()
+        assert ds.ncattrs() == ["globalattr1"]
+        assert ds.getncattr("globalattr1") == "one"
+        ds.close()
 
-    # Now readback
-    ds_back = from_nc4(filepath)
-    print("\nRead-back ncdata from file:")
-    print(ds_back)
+        # Now readback
+        ds_back = from_nc4(filepath)
+        print("\nRead-back ncdata from file:")
+        print(ds_back)
 
-    assert list(ds_back.dimensions.keys()) == ["dim_x", "dim_y"]
-    dimx, dimy = [ds_back.dimensions[name] for name in ("dim_x", "dim_y")]
-    assert dimx.size == len_x and dimx.unlimited
-    assert dimy.size == len_y and not dimy.unlimited
+        assert list(ds_back.dimensions.keys()) == ["dim_x", "dim_y"]
+        dimx, dimy = [ds_back.dimensions[name] for name in ("dim_x", "dim_y")]
+        assert dimx.size == len_x and dimx.unlimited
+        assert dimy.size == len_y and not dimy.unlimited
 
-    assert list(ds_back.variables.keys()) == ["var_x"]
-    varx = ds_back.variables["var_x"]
+        assert list(ds_back.variables.keys()) == ["var_x"]
+        varx = ds_back.variables["var_x"]
 
-    assert list(varx.attributes.keys()) == ["varattr1"]
-    assert varx.attributes["varattr1"].value == 1
+        assert list(varx.attributes.keys()) == ["varattr1"]
+        assert varx.attributes["varattr1"].value == 1
 
-    assert list(ds_back.attributes.keys()) == ["globalattr1"]
-    assert ds_back.attributes["globalattr1"].value == "one"
+        assert list(ds_back.attributes.keys()) == ["globalattr1"]
+        assert ds_back.attributes["globalattr1"].value == "one"
+
+    finally:
+        rmtree(tempdir_path)
+
+    print('\n== Netcdf4 save-load roundtrip finished OK.\n')
+
 
 
 if __name__ == "__main__":
