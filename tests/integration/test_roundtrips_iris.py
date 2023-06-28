40a24058d72e07386855f59c3b6f2a86c60f6821
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2023-06-28 09:38:37 +0100

    Rework standard-testcases fixture; prove thread-safety problem + fix.

--- tests/integration/test_roundtrips_iris.py
+++ tests/integration/test_roundtrips_iris.py
@@ -1,5 +1,5 @@
 """
-Test ncdata.iris by checking roundtrips for standard testcases.
+Test ncdata.iris by checking roundtrips for standard standard_testcases_func.
 
 Testcases start as netcdf files.
 (1) check equivalence of cubes : iris.load(file) VS iris.load(ncdata(file))
@@ -19,29 +19,47 @@ from ncdata.netcdf4 import from_nc4, to_nc4
 from tests._compare_nc_datasets import compare_nc_datasets
 from tests.data_testcase_schemas import standard_testcase, session_testdir
 
-from ncdata.iris import from_iris, to_iris
-
-# Avoid complaints that the fixture is unused
+# Avoid complaints that imported fixtures are "unused"
 standard_testcase, session_testdir
 
-# _Debug = True
-_Debug = False
+from ncdata.iris import from_iris, to_iris
 
 import iris.fileformats.netcdf._thread_safe_nc as ifnt
 
+_FIX_LOCKS = True
+# _FIX_LOCKS = False
+if _FIX_LOCKS:
+    @pytest.fixture(scope='session')
+    def use_irislock():
+        tgt = 'ncdata.netcdf4._GLOBAL_NETCDF4_LIBRARY_THREADLOCK'
+        with mock.patch(tgt, new=ifnt._GLOBAL_NETCDF4_LOCK):
+            yield
 
-@pytest.fixture(scope='session')
-def use_irislock():
-    tgt = 'ncdata.netcdf4._GLOBAL_NETCDF4_LIBRARY_THREADLOCK'
-    with mock.patch(tgt, new=ifnt._GLOBAL_NETCDF4_LOCK):
-        yield
+
+_TINY_CHUNKS = True
+# _TINY_CHUNKS = False
+if _TINY_CHUNKS:
+    # Note: from experiment, the test most likely to fail due to thread-safety is
+    #   "test_load_direct_vs_viancdata[testdata____testing__small_theta_colpex]"
+    # Resulting errors vary widely, including netcdf/HDF errors, data mismatches and
+    # segfaults.
+    # The following _CHUNKSIZE_SPEC makes it fail ~70% of runs (run as a single test)
+    # HOWEVER, the overall test runs get a LOT slower (e.g. 110sec --> )
+    _CHUNKSIZE_SPEC = "20Kib"
+    # HOWEVER, the above '_FIX_LOCKS' operation seems to prevent this.
+    @pytest.fixture(scope='session', autouse=True)
+    def force_tiny_chunks():
+        import dask.config as dcfg
+        with dcfg.set({"array.chunk-size": _CHUNKSIZE_SPEC}):
+            yield
 
 
 def test_load_direct_vs_viancdata(standard_testcase, use_irislock):
     source_filepath = standard_testcase.filepath
     ncdata = from_nc4(source_filepath)
 
-    _Debug = True
+    # _Debug = True
+    _Debug = False
     if _Debug:
         print(f"\ntestcase: {standard_testcase.name}")
         print("spec =")
@@ -57,8 +75,10 @@ def test_load_direct_vs_viancdata(standard_testcase, use_irislock):
     # Load same, via ncdata
     iris_ncdata_cubes = to_iris(ncdata)
     # Unfortunately, cube order is not guaranteed to be stable.
-    iris_cubes = sorted(iris_cubes, key=lambda cube: cube.name())
-    iris_ncdata_cubes = sorted(iris_ncdata_cubes, key=lambda cube: cube.name())
+    iris_cubes, iris_ncdata_cubes = (
+        sorted(cubes, key=lambda cube: cube.name())
+        for cubes in (iris_cubes, iris_ncdata_cubes)
+    )
 
     # There is also a peculiar problem with cubes that have all-masked data.
     # Let's just skip any like that, for now...
@@ -72,10 +92,10 @@ def test_load_direct_vs_viancdata(standard_testcase, use_irislock):
             if not all_maskeddata_cube(iris_cubes[i])
             and not all_maskeddata_cube(iris_ncdata_cubes[i])
         ]
-        iris_cubes = [cube for i, cube in enumerate(iris_cubes) if i in i_ok]
-        iris_ncdata_cubes = [
-            cube for i, cube in enumerate(iris_ncdata_cubes) if i in i_ok
-        ]
+        iris_cubes, iris_ncdata_cubes = (
+            [cube for i, cube in enumerate(cubes) if i in i_ok]
+            for cubes in (iris_cubes, iris_ncdata_cubes)
+        )
 
         n_cubes = len(iris_cubes)
         for i_cube in range(n_cubes):
@@ -97,6 +117,7 @@ def test_load_direct_vs_viancdata(standard_testcase, use_irislock):
         """
         if (
                 (c1.metadata == c2.metadata)
+                and (c1.shape == c2.shape)
                 and all(cube.dtype.kind in ('U', 'S') for cube in (c1, c2))
         ):
             # cludge comparison for string-type cube data
@@ -113,15 +134,14 @@ def test_load_direct_vs_viancdata(standard_testcase, use_irislock):
     expected = [(cube.name(), True) for cube in iris_cubes]
     result = results == expected
 
-
-
     if not result:
         # FOR NOW: compare with experimental ncdata comparison.
-        # I know this is a bit circular, but can use for debugging, for now ...
+        # I know this is a bit circular, but it is useful for debugging, for now ...
         result = compare_nc_datasets(
             from_iris(iris_cubes), from_iris(iris_ncdata_cubes)
         )
         assert result == []
+
     # assert iris_cubes == iris_ncdata_cubes
     assert result
 
@@ -144,7 +164,8 @@ def test_save_direct_vs_viancdata(standard_testcase, tmp_path):
     temp_ncdata_savepath = tmp_path / "temp_save_iris_via_ncdata.nc"
     to_nc4(from_iris(iris_cubes), temp_ncdata_savepath)
 
-    _Debug = True
+    # _Debug = True
+    _Debug = False
     if _Debug:
         print(f"\ntestcase: {standard_testcase.name}")
         print("spec =")
