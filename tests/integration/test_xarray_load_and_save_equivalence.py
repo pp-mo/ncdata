4d4ba0c8dfbb34984b3ad5f8ab4339faf65f23d7
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2024-03-17 15:35:42 +0000

    WIP tests etc

--- tests/integration/test_xarray_load_and_save_equivalence.py
+++ tests/integration/test_xarray_load_and_save_equivalence.py
@@ -5,6 +5,7 @@ Testcases start as netcdf files.
 (1) check equivalence of cubes : xarray.load(file) VS xarray.load(ncdata(file))
 (2) check equivalence of files : xarray -> file VS xarray->ncdata->file
 """
+import numpy as np
 from subprocess import check_output
 
 import pytest
@@ -52,7 +53,9 @@ def test_load_direct_vs_viancdata(
     source_filepath = standard_testcase.filepath
     ncdata = from_nc4(source_filepath)
 
-    if standard_testcase.name in BAD_LOADSAVE_TESTCASES["xarray"]["load"]:
+    excluded_cases = BAD_LOADSAVE_TESTCASES["xarray"]["load"]
+    excluded_cases.extend(BAD_LOADSAVE_TESTCASES["xarray"]["save"])
+    if standard_testcase.name in excluded_cases:
         pytest.skip("excluded testcase (xarray cannot load)")
 
     # _Debug = True
@@ -69,15 +72,56 @@ def test_load_direct_vs_viancdata(
 
     # Load the testcase with Xarray.
     xr_ds = xarray.open_dataset(source_filepath, chunks=-1)
+
     # Load same, via ncdata
     xr_ncdata_ds = to_xarray(ncdata)
 
-    # Xarray dataset (variable) comparison is problematic
-    # result = xr_ncdata_ds.identical(xr_ds)
+    testvar_names = None
+    if (standard_testcase.name == "ds_testdata1") or ("toa_brightness" in standard_testcase.name):
+        testvar_names = ['time']
+    elif standard_testcase.name == r"testdata__\lambert_azimuthal_equal_area\euro_air_temp":
+        testvar_names = ["time", "forecast_reference_time"]
+        # testvar_names = ["time"]
+        # testvar_names = ["forecast_reference_time"]
+    elif "theta_nodal" in standard_testcase.name:
+        testvar_names = ["Mesh0"]
+
+    if testvar_names:
+        # print("XR ds")
+        # print(xr_ds)
+        #
+        # print("")
+        # print("xrds == xrncds ?", xr_ds.identical(xr_ncdata_ds))
+
+        for testvar_name in testvar_names:
+            print(f"\nxr_ds['{testvar_name}']:\n", xr_ds[testvar_name])
+            print(f"xr_ncdata_ds['{testvar_name}']:\n", xr_ncdata_ds[testvar_name])
+            print(f"xr_ds['{testvar_name}'].encoding['units']:\n", xr_ds[testvar_name].encoding.get('units'))
+            print(f"xr_ncdata_ds['{testvar_name}'].encoding['units']:\n", xr_ncdata_ds[testvar_name].encoding.get('units'))
+            # Xarray dataset (variable) comparison is problematic
+            # result = xr_ncdata_ds.identical(xr_ds)
+
+            # do_fix = "none"
+            # do_fix = "fix_xrds"
+            # do_fix = "fix_xrncds"
+            do_fix = "fix_origshape"
+            if do_fix == "fix_xrncds":
+                print('\nOLD xrncds data:\n', xr_ncdata_ds[testvar_name].data)
+                xr_ncdata_ds[testvar_name].data = xr_ncdata_ds[testvar_name].data.compute()
+                print('NEW xrncds data:\n', xr_ncdata_ds[testvar_name].data)
+            elif do_fix == "fix_xrds":
+                import dask.array as da
+                print('\nOLD xrds data:\n', xr_ds[testvar_name].data)
+                data = xr_ds[testvar_name].data
+                xr_ds[testvar_name].data = da.from_array(data, meta=np.ndarray((), dtype=data.dtype), chunks=-1)
+                print('NEW xrds data:\n', xr_ds[testvar_name].data)
+            elif do_fix == "fix_origshape":
+                xr_ncdata_ds[testvar_name].encoding['original_shape'] = ()
 
     # So for now, save Xarray datasets to disk + compare that way.
     temp_xr_path = tmp_path / "tmp_out_xr.nc"
     temp_xr_ncdata_path = tmp_path / "tmp_out_xr_ncdata.nc"
+
     xr_ds.to_netcdf(temp_xr_path)
     xr_ncdata_ds.to_netcdf(temp_xr_ncdata_path)
 
@@ -110,6 +154,9 @@ def test_save_direct_vs_viancdata(standard_testcase, tmp_path):
     ncdata = from_nc4(source_filepath)
 
     excluded_testcases = BAD_LOADSAVE_TESTCASES["xarray"]["load"]
+    excluded_testcases.extend(BAD_LOADSAVE_TESTCASES["xarray"]["save"])
+    for excl in excluded_testcases:
+        print('  ', excl)
     if any(key in standard_testcase.name for key in excluded_testcases):
         pytest.skip("excluded testcase")
 
