4f85741719dc7b48fe986c87edc9336b2ef18b97
Martin Yeo <martin.yeo@metoffice.gov.uk>
2024-05-31 12:56:30 +0100

    Add a logo.

--- docs/conf.py
+++ docs/conf.py
@@ -100,9 +100,17 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+html_logo = "_static/logo.png"
+html_favicon = "_static/logo.png"
 html_theme = "pydata_sphinx_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+html_theme_options = {
+    "logo": {
+        "text": "ncdata documentation",
+    }
+}
