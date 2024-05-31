5a31f58582d87015b4ae12f43908993eb88c98b3
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2024-05-31 11:37:09 +0100

    Revise comparison API; make variable-differences public.

--- lib/ncdata/utils/__init__.py
+++ lib/ncdata/utils/__init__.py
@@ -1,6 +1,10 @@
 """General user utility functions."""
 
-from ._compare_nc_datasets import dataset_differences
+from ._compare_nc_datasets import dataset_differences, variable_differences
 from ._save_errors import save_errors
 
-__all__ = ["dataset_differences", "save_errors"]
+__all__ = [
+    "dataset_differences",
+    "save_errors",
+    "variable_differences",
+]
