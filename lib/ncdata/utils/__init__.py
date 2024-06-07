b29f26684a056720f9b5fabc14038ed1955decca
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2024-06-08 00:49:00 +0100

    Fix order of presentation of utils routines.

--- lib/ncdata/utils/__init__.py
+++ lib/ncdata/utils/__init__.py
@@ -4,7 +4,7 @@ from ._compare_nc_datasets import dataset_differences, variable_differences
 from ._save_errors import save_errors
 
 __all__ = [
-    "dataset_differences",
     "save_errors",
+    "dataset_differences",
     "variable_differences",
 ]
