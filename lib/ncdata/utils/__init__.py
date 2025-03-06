68c121add942aef393da84ecfdcc2ef66ca27aaf
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2025-03-06 14:16:19 +0000

    Initial dim slicing: WIP no groups handling, Slicer untested as yet.

--- lib/ncdata/utils/__init__.py
+++ lib/ncdata/utils/__init__.py
@@ -1,10 +1,13 @@
 """General user utility functions."""
 from ._compare_nc_datasets import dataset_differences, variable_differences
 from ._copy import ncdata_copy
+from ._dim_indexing import Slicer, index_by_dimensions
 from ._save_errors import save_errors
 
 __all__ = [
+    "Slicer",
     "dataset_differences",
+    "index_by_dimensions",
     "ncdata_copy",
     "save_errors",
     "variable_differences",
